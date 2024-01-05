0c22cf3917867162175687f20185e8183ad00448
dani <dnlvstn@gmail.ru>
2024-01-05 19:40:53 +1200

    bitstring done

--- src/secondLaba/BitString.java
+++ src/secondLaba/BitString.java
@@ -1,10 +1,5 @@
 package secondLaba;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class BitString {
 
@@ -12,24 +7,82 @@ public class BitString {
     private int len;
 
     public boolean get(int index) {
+        if (index / Long.SIZE > 0) {
+            expand(index / Long.SIZE + 1);
+        }
         return (data[index / Long.SIZE] << (index % Long.SIZE)) < 0;
     }
 
     public void reset(int index) {
-        int longIndex = (len - index - 1) / Long.SIZE;
+        int longIndex = index / Long.SIZE;
         int bitIndex = index % Long.SIZE;
-        long mask = 1L << bitIndex;
+        long mask = 1L << Long.SIZE - bitIndex - 1;
         data[longIndex] &= ~mask;
     }
 
     public void set(int index) {
+        if (index >= len) {
+            len = index + 1;
+        }
         int longIndex = index / Long.SIZE;
         int bitIndex = index % Long.SIZE;
-        long mask = 1L << len - bitIndex - 1;
-        if (longIndex >= 0) {
+        long mask = 1L << Long.SIZE - bitIndex - 1;
+        if (longIndex > 0) {
             expand(longIndex + 1);
         }
         data[longIndex] |= mask;
+    }
+
+    public BitString and(BitString second) {
+        BitString res = new BitString("");
+        int lim = Math.max(this.len, second.len);
+        res.expand(lim / Long.SIZE + 1);
+        res.len = lim;
+        for (int i = 0; i < lim; i++) {
+            if (this.get(i) && second.get(i)) {
+                res.set(i);
+            }
+        }
+        return res;
+    }
+
+
+    public BitString or(BitString second) {
+        BitString res = new BitString("");
+        int lim = Math.max(this.len, second.len);
+        res.expand(lim / Long.SIZE + 1);
+        res.len = lim;
+        for (int i = 0; i < lim; i++) {
+            if (this.get(i) || second.get(i)) {
+                res.set(i);
+            }
+        }
+        return res;
+    }
+
+    public BitString not() {
+        for (int i = 0; i < len; i++) {
+            if (this.get(i)) {
+                this.reset(i);
+            } else {
+                this.set(i);
+            }
+        }
+        return this;
+    }
+
+
+    public BitString xor(BitString second) {
+        BitString res = new BitString("");
+        int lim = Math.max(this.len, second.len);
+        res.expand(lim / Long.SIZE + 1);
+        res.len = lim;
+        for (int i = 0; i < lim; i++) {
+            if (this.get(i) != second.get(i)) {
+                res.set(i);
+            }
+        }
+        return res;
     }
 
     private void expand(int newLength) {
@@ -41,153 +94,46 @@ public class BitString {
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
+        var lim = len;
         for (long l : data) {
             for (long mask = 1L << 63; mask != 0; mask >>>= 1) {
-                res.append((l & mask) == 0 ? "0" : "1");
+                res.append(((l & mask) == 0 || (l == 0)) ? "0" : "1");
+                lim--;
+                if (lim <= 0) break;
             }
             res.append(" ");
         }
         return res.toString();
     }
 
-    public static void setTest() {
-        var test = new BitString("");
-        test.set(0);
-        test.set(63);
-        test.set(64);
-        System.out.println(test);
-    }
-
 
     public BitString(String string) {
         len = string.length();
+        if (len / Long.SIZE > 0) {
+            this.expand(len / Long.SIZE + 1);
+        }
         char[] chars = string.toCharArray();
         for (int i = 0; i < len; i++) {
             if (chars[i] == '1') set(i);
         }
-//        if ((len % Long.SIZE) > 0) {
-//            data[len / Long.SIZE] >>>= (Long.SIZE - len);
-//        }
-
     }
-
-
-//        int i = 0;
-//        while (!string.isEmpty()) {
-//            int index = string.length() / Long.SIZE;
-//            if (index > data.length - 1) {
-//                expand(index + 1);
-//            }
-//            int end = index > 0 ? Long.SIZE : string.length();
-//            data[i] = Long.parseLong(string.substring(0, end));
-//            try {
-//                string = string.substring(end, end + (string.length() % Long.SIZE));
-//            } catch (IndexOutOfBoundsException e) {
-//                break;
-//            }
-//            i++;
-//        }
-//
-//    @Override
-//    public String toString() {
-//        return this.data.stream().map(Object::toString).collect(Collectors.joining(""));
-//    }
-//
-//    private static void equate(BitString first, BitString second) {
-//        int diff = first.data.size() - second.data.size();
-//        if (first.data.size() > second.data.size()) {
-//            for (; diff > 0; diff--) second.data.add(0, 0);
-//        }
-//        else {
-//            for (; diff > 0; diff--) first.data.add(0, 0);
-//        }
-//    }
-//
-//    private void removeZeros() {
-//        while (this.data.get(0) == 0) {
-//            this.data.remove(0);
-//        }
-//    }
-//
-//    public BitString and(BitString second) {
-//        equate(this, second);
-//        for (int i = 0; i < this.data.size(); i++) {
-//            if ((this.data.get(i) + second.data.get(i)) > 0) {
-//                this.data.set(i, 1);
-//            }
-//        }
-//        this.removeZeros();
-//        return this;
-//    }
-//
-//    public BitString or(BitString second) {
-//        equate(this, second);
-//        for (int i = 0; i < this.data.size(); i++) {
-//            if ((this.data.get(i) * second.data.get(i)) == 1) {
-//                this.data.set(i, 1);
-//            }
-//            else {
-//                this.data.set(i, 0);
-//            }
-//        }
-//        this.removeZeros();
-//        return this;
-//    }
-//
-//    public static BitString and(BitString first, BitString second) {
-//        equate(first, second);
-//        BitString res = new BitString();
-//        for (int i = 0; i < first.data.size(); i++) {
-//            res.data.add((first.data.get(i) + second.data.get(i)) > 0 ? 1 : 0);
-//        }
-//        first.removeZeros();
-//        return res;
-//    }
-//
-//    public static BitString or(BitString first, BitString second) {
-//        equate(first, second);
-//        BitString res = new BitString();
-//        for (int i = 0; i < first.data.size(); i++) {
-//            res.data.add((first.data.get(i) * second.data.get(i)) == 1 ? 1 : 0);
-//        }
-//        res.removeZeros();
-//        return res;
-//    }
-//
-//    public BitString not() {
-//        this.data.replaceAll(integer -> integer == 0 ? 1 : 0);
-//        return this;
-//    }
-//
-//    public static BitString xor(BitString first, BitString second) {
-//        equate(first, second);
-//        BitString res = new BitString();
-//        for (int i = 0; i < first.data.size(); i++) {
-//            res.data.add(!Objects.equals(first.data.get(i), second.data.get(i)) ? 1 : 0);
-//        }
-//        res.removeZeros();
-//        return res;
-//    }
-//
-//    public BitString set(int index, int bit) {
-//        this.data.set(index, bit);
-//        return this;
-//    }
 
     public static void main(String[] args) {
         var first = new BitString("10101001");
-        var second = new BitString("10111001010101010010100101010100101010101010101111111000000000101001001001010010001");
+        var second = new BitString("11100000000000000000000000000000000000000000000000000000000000010100000000001");
         var third = new BitString("1111111111111111111111111111111111111111111111111111111111111111000000");
         System.out.printf("First is %s\n", first);
         System.out.printf("Second is %s\n", second);
         System.out.printf("Third is %s\n", third);
-        first.reset(3);
-        second.reset(33);
+        first.reset(2);
+        second.reset(2);
+        third.set(70);
         System.out.printf("First is %s\n", first);
         System.out.printf("Second is %s\n", second);
         System.out.printf("Third is %s\n", third);
-//
-//        setTest();
+
+        System.out.println(first.xor(second));
+
     }
 
 }
